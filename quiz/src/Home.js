8cdbe5f2a1b48128f01b8c3f8e91ecc849848703
Brennen <bhogan1@nd.edu>
2019-11-08 22:46:17 -0500

    homepage

--- quiz/src/Home.js
+++ quiz/src/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import char from './images/char.jpg'
+import './home.css'
+import {Link} from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <div>Hi</div>
-        <p>whts up</p>
-      </div>
+        <h1>Title</h1>
+        <img src={char} alt="char"/>
+        <p className="p1">Don’t know which candidate to vote for?</p>
+        <p className="p2">Decide which quotes you agree with most on many topics topics in this dynamically generated quiz to discern which candidate represents your views best.</p>
+        <Link to='/topics'> 
+            <button type="button" className="button"><b>Start Quiz</b></button>
+        </Link>
+        </div>
+
     )
   }
 }
