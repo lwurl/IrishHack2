b9aea8929eeba99780a954de5adecf2ac65c81a1
Luke Wurl <lwurl@nd.edu>
2019-11-09 16:43:32 -0500

    detailed results works

--- quiz/src/detailedResults.js
+++ quiz/src/detailedResults.js
@@ -1,10 +1,95 @@
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import './detailedResults.css';
 import {Link} from 'react-router-dom';
 import { db } from './firebase';
 
 class detailedResults extends React.Component {
+    state = {
+        responses: {},
+        quotes: {}
+    };
+
+    async componentDidMount() {
+        await this.getResponses();
+    }
+
+    getResponses = async () => {
+        let responses = {};
+        await db.collection("users").get().then((querySnapshot) => {
+            // only one user for now
+            querySnapshot.forEach((doc) => {
+                responses = doc.data()['responses'];
+            });
+        });
+        let quotes = {};
+        await db.collection("quotes").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                quotes[doc.id] = doc.data();
+            })
+        });
+        this.setState({ responses, quotes });
+    }
+
+    convertName = (name) => {
+        switch (name) {
+            case 'sanders':
+                return 'Bernie Sanders';
+            case 'biden':
+                return 'Joe Biden';
+            case 'warren':
+                return 'Elizabeth Warren';
+            case 'trump':
+                return 'Donald Trump';
+            case 'pete':
+                return 'Pete Buttigieg';
+        }
+        return '';
+    }
+
+    printQuotes = () => {
+        let returns = [];
+        for (var topic in this.state.responses) {
+            console.log(topic);
+            for (var question in this.state.responses[topic]) {
+                // console.log(question);
+                if (this.state.responses[topic][question]['answered'] === true) {
+                    console.log(this.state.responses[topic][question]);
+                    let sortedRanks = [];
+
+                    for (var key in this.state.responses[topic][question]) {
+                        if (this.state.responses[topic][question].hasOwnProperty(key) && key !== 'answered') {
+                            sortedRanks.push( [ key, this.state.responses[topic][question][key] ] );
+                        }
+                    }
+
+                    sortedRanks.sort((a, b) => a[1] - b[1]);
+                    console.log(sortedRanks);
+
+
+                    // console.log(this.state.quotes[topic][question][sortedRanks[0][0]]);
+
+
+                    returns.push(<Fragment>
+                            <hr></hr>
+                            <h2>{this.state.quotes[topic][question]['blurb']}</h2>
+                            <div class="subtext">{this.state.quotes[topic][question]['desc']}
+                            </div>
+                            
+                            <ul>
+                                <li><span class="number">1: {this.convertName(sortedRanks[0][0])}</span><span class="box quote">{this.state.quotes[topic][question][sortedRanks[0][0]]}</span></li>
+                                <li><span class="number">2: {this.convertName(sortedRanks[1][0])}</span><span class="box quote">{this.state.quotes[topic][question][sortedRanks[1][0]]}</span></li>
+                                <li><span class="number">3: {this.convertName(sortedRanks[2][0])}</span><span class="box quote">{this.state.quotes[topic][question][sortedRanks[2][0]]}</span></li>
+                                <li><span class="number">4: {this.convertName(sortedRanks[3][0])}</span><span class="box quote">{this.state.quotes[topic][question][sortedRanks[3][0]]}</span></li>
+                                <li><span class="number">5: {this.convertName(sortedRanks[4][0])}</span><span class="box quote">{this.state.quotes[topic][question][sortedRanks[4][0]]}</span></li>
+                            </ul>
+                            </Fragment>);
+                        // </Fragment>
+                }
+            }
+        }
+        return returns;
+    }
 
     render() {
       return (
@@ -12,19 +97,9 @@ class detailedResults extends React.Component {
             <h1>Detailed Results</h1>
             <div class="subtext">Check out your quote rankings for each question and see which candidate said each quote. 
             </div>
-            <hr></hr>
-            <h2>Minimum Wage</h2>
-            
-            <div class="subtext">Congress hasn't legislated a national minimum-wage since 2007 when it hiked the hourly minimum wage to $7.25 effective July 2009. Many states have since mandated a minimum wage that is higher than the federal minimum wage. Some argue it's time to increase the minimum wage on a national scale to help lift people out of povery and decrease income inequality. Others believe that a minimum wage increase hurts small businesses and encourage companies to downsize their staff.
-            </div>
-            <ul>
-                <li> <span class="number">1:Joe Biden</span>
-                <span class="box quote">It's well past time that we increase the federal minimum wage to $15</span></li>
-                <li><span class="number">2: Elizabeth Warren</span><span class="box quote">We should commit to requiring federal contractors to pay workers a minimum wage of $15 an hour</span></li>
-                <li><span class="number">3: Bernie Sanders</span><span class="box quote">Provide domestic workers with at least $15 minimum wage, strong protections for collective bargaining, workers' rights, and workplace safety</span></li>
-                <li><span class="number">4: Pete Buttigeg</span><span class="box quote">Raise the minimum wage to $15 and ensure workers access to the predictable hours, wages, and support they deserve.</span></li>
-                <li><span class="number">5: Donald Trump</span><span class="box quote">I do not support a Federal Miniumum wage --  I’d rather have the states go out and do what they have to do.  I don’t know how you live on $7.25 an hour, but I would say: let the states decide.</span></li>
-            </ul>
+            {
+                this.printQuotes()
+            }
         </div>
 
         )   
