671519cfbc411aefe26e24a3a54b4b255ffbd5ce
Brennen <bhogan1@nd.edu>
2019-11-09 11:06:59 -0500

    fonts and such

--- quiz/src/Home.js
+++ quiz/src/Home.js
@@ -8,8 +8,17 @@ class Home extends React.Component {
     return (
       <div>
         <img src={logo} alt="logo"/>
-        <p className="p1">Don’t know which candidate to vote for?</p>
-        <p className="p2">Decide which quotes you agree with most on many topics topics in this dynamically generated quiz to discern which candidate represents your views best.</p>
+
+
+
+        <p className="p1a"><b>4 out of 10 eligible voters do not vote.</b></p>
+        <p className="p1b">How can we motivate the uninformed voter to find a candidate that represents them in quick, easy, and engaging way?</p>
+        <p className="p1a"><b>There are currently over 20 major party candidates asking us to vote for them in the 2020 election to be our president.</b> </p>
+        <p className="p1b">How can we discern which candidate best represents our views on the issues that are most important to us? </p>
+        <p className="p1a"><b>Over 2 million articles are published on the web everyday and with the election being one of the hottest topics. </b></p>
+        <p className="p1b">How can we avoid the bias of media summaries and look directly at what candidates are saying? </p>
+        <hr></hr>
+        <p className="p2">Welcome to Quote Vote, a web app that helps you understand what candidates you align with. Reflect on what issues are most important to you and get a customized, dynamically generated quiz where you rank anonymized quotes from candidates. Afterwards, see who said what and view your results to discern  which candidate best represents your views. </p>
         <Link to='/topics'> 
           <div className="center_div"><button type="button" className="buttonMain"><b>Start Quiz</b></button></div>
         </Link>
