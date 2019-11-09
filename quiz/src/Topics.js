cc85fec2354eb5a5a1ffae9ccc302541f56289b3
Luke Wurl <lwurl@nd.edu>
2019-11-09 13:49:20 -0500

    topics work

--- quiz/src/Topics.js
+++ quiz/src/Topics.js
@@ -6,7 +6,7 @@ import econ from './images/economy.svg'
 import edu from './images/education.svg'
 import hc from './images/hc.svg'
 import env from './images/env.svg'
-
+import { db } from './firebase';
 
 
 class Topics extends React.Component {
@@ -14,23 +14,40 @@ class Topics extends React.Component {
     super(props);
     this.state = {
       percentages: {},
-      value = 0
-    };
-    handleChange: function(event) {
-      this.setState({value: event.target.value});
+      topics: {
+        economy: 50,
+        education: 50,
+        environment: 50,
+        guncontrol: 50,
+        healthcare: 50
+    }
     };
   }
 
-  async componentDidMount() {
-    await this.getSliders();
+  handleChange = (event, val) => {
+    let topics = this.state.topics;
+    topics[val] = parseInt(event.target.value)-1;
+    this.setState({ topics });
   }
 
-  getSliders = async () => {
-    var percentages = {};
-    percentages['economy'] = document.getElementById("economy").value;
-    this.setState({ percentages });
-
-    console.log(this.state.percentages);
+  addTopicPreferences = async () => {
+    let doc_id;
+    await db.collection("users").get().then((querySnapshot) => {
+      // only one user for now
+      querySnapshot.forEach((doc) => {
+        doc_id = doc.id;
+      });
+    });
+    await db.collection("users").doc(doc_id).update({
+      categories: this.state.topics
+    })
+    .then(function() {
+      console.log("Document successfully updated!");
+    })
+    .catch(function(error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+    });
   }
 
   render() {
@@ -39,62 +56,62 @@ class Topics extends React.Component {
         <h1>Topics</h1>
 
         <img src={econ} alt="gun" className='icon'/>
-        <h2 class="topic_title">Economy</h2>
-        <div class="slidecontainer">
-        <input id="economy" type="range" min="1" max="101" defaultValue="50" class="slider" id="myRange1" step="25" list="range-labels" value={this.state.value} onChange={this.handleChange}/>
-        <div class="label_div not">Not at all important</div>
-        <div class="label_div low">Low importance</div>
-        <div class="label_div neutral">Neutral</div>
-        <div class="label_div important">Important</div>
-        <div class="label_div very">Very important</div> 
+        <h2 className="topic_title">Economy</h2>
+        <div className="slidecontainer">
+        <input type="range" min="1" max="101" className="slider" id="myRange1" step="25" list="range-labels" value={this.state.topics.economy} onChange={(ev) => this.handleChange(ev, 'economy')}/>
+        <div className="label_div not">Not at all important</div>
+        <div className="label_div low">Low importance</div>
+        <div className="label_div neutral">Neutral</div>
+        <div className="label_div important">Important</div>
+        <div className="label_div very">Very important</div> 
         </div>
 
         <img src={edu} alt="gun" className='icon'/>
-        <h2 class="topic_title">Education</h2>
-        <div class="slidecontainer">
-        <input type="range" min="1" max="101" defaultValue="50" class="slider" id="myRange2" step="25" list="range-labels"/>
-        <div class="label_div not">Not at all important</div>
-        <div class="label_div low">Low importance</div>
-        <div class="label_div neutral">Neutral</div>
-        <div class="label_div important">Important</div>
-        <div class="label_div very">Very important</div> 
+        <h2 className="topic_title">Education</h2>
+        <div className="slidecontainer">
+        <input type="range" min="1" max="101" className="slider" id="myRange2" step="25" list="range-labels" value={this.state.topics.education} onChange={(ev) => this.handleChange(ev, 'education')}/>
+        <div className="label_div not">Not at all important</div>
+        <div className="label_div low">Low importance</div>
+        <div className="label_div neutral">Neutral</div>
+        <div className="label_div important">Important</div>
+        <div className="label_div very">Very important</div> 
         </div>
 
         <img src={env} alt="gun" className='icon'/>
-        <h2 class="topic_title">Environment</h2>
-        <div class="slidecontainer">
-        <input type="range" min="1" max="101" defaultValue="50" class="slider" id="myRange3" step="25" list="range-labels"/>
-        <div class="label_div not">Not at all important</div>
-        <div class="label_div low">Low importance</div>
-        <div class="label_div neutral">Neutral</div>
-        <div class="label_div important">Important</div>
-        <div class="label_div very">Very important</div> 
+        <h2 className="topic_title">Environment</h2>
+        <div className="slidecontainer">
+        <input type="range" min="1" max="101" className="slider" id="myRange3" step="25" list="range-labels" value={this.state.topics.environment} onChange={(ev) => this.handleChange(ev, 'environment')}/>
+        <div className="label_div not">Not at all important</div>
+        <div className="label_div low">Low importance</div>
+        <div className="label_div neutral">Neutral</div>
+        <div className="label_div important">Important</div>
+        <div className="label_div very">Very important</div> 
         </div>
 
         <img src={gun} alt="gun" className='icon'/>
-        <h2 class="topic_title">Gun Control</h2>
-        <div class="slidecontainer">
-        <input type="range" min="1" max="101" defaultValue="50" class="slider" id="myRange4" step="25" list="range-labels"/>
-        <div class="label_div not">Not at all important</div>
-        <div class="label_div low">Low importance</div>
-        <div class="label_div neutral">Neutral</div>
-        <div class="label_div important">Important</div>
-        <div class="label_div very">Very important</div> 
+        <h2 className="topic_title">Gun Control</h2>
+        <div className="slidecontainer">
+        <input type="range" min="1" max="101" className="slider" id="myRange4" step="25" list="range-labels" value={this.state.topics.guncontrol} onChange={(ev) => this.handleChange(ev, 'guncontrol')}/>
+        <div className="label_div not">Not at all important</div>
+        <div className="label_div low">Low importance</div>
+        <div className="label_div neutral">Neutral</div>
+        <div className="label_div important">Important</div>
+        <div className="label_div very">Very important</div> 
         </div>
 
         <img src={hc} alt="gun" className='icon'/>
-        <h2 class="topic_title">Health Care</h2>
-        <div class="slidecontainer">
-        <input type="range" min="1" max="101" defaultValue="50" class="slider" id="myRange5" step="25" list="range-labels"/>
-        <div class="label_div not">Not at all important</div>
-        <div class="label_div low">Low importance</div>
-        <div class="label_div neutral">Neutral</div>
-        <div class="label_div important">Important</div>
-        <div class="label_div very">Very important</div> 
+        <h2 className="topic_title">Health Care</h2>
+        <div className="slidecontainer">
+        <input type="range" min="1" max="101" className="slider" id="myRange5" step="25" list="range-labels" value={this.state.topics.healthcare} onChange={(ev) => this.handleChange(ev, 'healthcare')}/>
+        <div className="label_div not">Not at all important</div>
+        <div className="label_div low">Low importance</div>
+        <div className="label_div neutral">Neutral</div>
+        <div className="label_div important">Important</div>
+        <div className="label_div very">Very important</div> 
         </div>
 
         <Link to='/quiz'> 
-          <div className="center_div"><button type="button" className="buttonTop"><b>Take Quiz</b></button></div>
+          <div className="center_div"><button type="button" className="buttonTop" onClick={() => this.addTopicPreferences()}><b>Take Quiz</b></button></div>
         </Link>
       </div>
     )
